d08299847ffe3a7b03a24266d0224dcae43bea0e
Shane Hussel <shussel@gmail.com>
2023-12-15 18:08:02 -0500

    Add mongo password service provider

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         MongoDB\Laravel\MongoDBServiceProvider::class,
+        MongoDB\Laravel\Auth\PasswordResetServiceProvider::class,
     ])->toArray(),
 
     /*
