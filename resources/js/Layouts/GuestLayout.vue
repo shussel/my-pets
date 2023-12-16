7d4f72fcfe496fb55b54844ce5c93e25d4d3dc0f
Shane Hussel <shussel@gmail.com>
2023-12-16 02:03:54 -0500

    Update welcome view

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -1,20 +1,23 @@
-<script setup>
+<script setup xmlns="http://www.w3.org/1999/html">
 import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-        <div>
-            <Link href="/">
-                <ApplicationLogo class="fill-current text-gray-500" />
-            </Link>
-        </div>
-
         <div
             class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg"
         >
+            <div>
+                <Link href="/">
+                    <ApplicationLogo class="fill-current text-gray-500" />
+                </Link>
+            </div>
+            <div
+                class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white"
+            >
             <slot />
+            </div>
         </div>
     </div>
 </template>
