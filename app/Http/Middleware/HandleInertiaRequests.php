dca1f6399fa99b5e928bde02a339d02d6a21e45e
Shane Hussel <shussel@gmail.com>
2024-01-03 01:19:14 -0500

    limit inertia shared auth info

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -33,7 +33,7 @@ class HandleInertiaRequests extends Middleware
         return [
             ...parent::share($request),
             'auth' => [
-                'user' => $request->user(),
+                'user' => $request->user()->only('_id', 'name', 'email'),
             ],
             'ziggy' => fn () => [
                 ...(new Ziggy)->toArray(),
